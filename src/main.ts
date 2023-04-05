f76e9403aae2883780facc77d13450b50c819e63
Erik Jensen <ejensen@truemark.io>
2023-04-04 21:12:47 -0600

    Adding sleep

--- src/main.ts
+++ src/main.ts
@@ -6,33 +6,44 @@ async function sleep(ms: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
 
+export async function waitForInstances(ec2Client: EC2Client, instanceIds: string[], timeout: number): Promise<void> {
+  console.log(`Waiting for instances ${instanceIds} to be ready`)
+  const command = new DescribeInstanceStatusCommand({
+    InstanceIds: instanceIds
+  })
+  const timeoutDate = new Date().getTime() + timeout
+  let status = false
+  do {
+    if (new Date().getTime() > timeoutDate) {
+      throw new Error('Timeout exceeded')
+    }
+    let output = await ec2Client.send(command)
+    if (output.InstanceStatuses === undefined || output.InstanceStatuses.length === 0) {
+      // sometimes AWS returns an empty list, so we need to retry
+      await sleep(3000)
+      output = await ec2Client.send(command)
+      if (output.InstanceStatuses === undefined || output.InstanceStatuses.length === 0) {
+        throw new Error('No instances found')
+      }
+    }
+    status = output.InstanceStatuses.map(
+      instance =>
+        instance.InstanceState?.Name === 'running' &&
+        instance.SystemStatus?.Status === 'ok' &&
+        instance.InstanceStatus?.Status === 'ok'
+    ).every(Boolean)
+    if (!status) {
+      await sleep(3000)
+    }
+  } while (!status)
+  console.log(`Instances ${instanceIds} are ready`)
+}
+
 async function run(): Promise<void> {
   try {
     const config = loadConfig()
-    console.log(`Waiting for instances ${config.instanceIds} to be ready`)
     const ec2Client = new EC2Client({region: config.region})
-    const command = new DescribeInstanceStatusCommand({
-      InstanceIds: config.instanceIds
-    })
-    const output = await ec2Client.send(command)
-    if (output.InstanceStatuses === undefined || output.InstanceStatuses.length === 0) {
-      throw new Error('No instances found')
-    }
-    const timeout = new Date().getMilliseconds() + config.timeout
-    let status = false
-    do {
-      if (new Date().getMilliseconds() < timeout) {
-        throw new Error('Timeout exceeded')
-      }
-      status = output.InstanceStatuses.map(
-        instance =>
-          instance.InstanceState?.Name === 'running' &&
-          instance.SystemStatus?.Status === 'ok' &&
-          instance.InstanceStatus?.Status === 'ok'
-      ).reduce((acc, cur) => acc && cur, true)
-      await sleep(5000)
-    } while (!status)
-    console.log(`Instances ${config.instanceIds} are ready`)
+    await waitForInstances(ec2Client, config.instanceIds, config.timeout)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
