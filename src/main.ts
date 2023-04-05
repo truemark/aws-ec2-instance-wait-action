3d9ec61c0194b308164254d7ee5f6ca750e5bd65
Erik Jensen <ejensen@truemark.io>
2023-04-04 20:44:27 -0600

    Adding initial changes

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,36 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {DescribeInstanceStatusCommand, EC2Client} from '@aws-sdk/client-ec2'
+import {loadConfig} from './config'
+
+async function sleep(ms: number): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const config = loadConfig()
+    const ec2Client = new EC2Client({region: config.region})
+    const command = new DescribeInstanceStatusCommand({
+      InstanceIds: config.instanceIds
+    })
+    const output = await ec2Client.send(command)
+    if (output.InstanceStatuses === undefined || output.InstanceStatuses.length === 0) {
+      throw new Error('No instances found')
+    }
+    const timeout = new Date().getMilliseconds() + config.timeout
+    let status = false
+    do {
+      if (new Date().getMilliseconds() < timeout) {
+        throw new Error('Timeout exceeded')
+      }
+      status = output.InstanceStatuses.map(
+        instance =>
+          instance.InstanceState?.Name === 'running' &&
+          instance.SystemStatus?.Status === 'ok' &&
+          instance.InstanceStatus?.Status === 'ok'
+      ).reduce((acc, cur) => acc && cur, true)
+      await sleep(5000)
+    } while (!status)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
